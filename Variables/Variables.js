67d5a6ed2323767c19984a76397470c58fb1020a
JirkaDellOro <del@hs-furtwangen.de>
2025-01-18 14:37:13 +0100

    react to change and added titles

--- Variables/Variables.js
+++ Variables/Variables.js
@@ -2,11 +2,12 @@
 window.addEventListener("load", start);
 window.addEventListener("dragstart", dragStart);
 window.addEventListener("drop", drop);
+window.addEventListener("change", change);
 let dropTargets;
 let dragSources;
 function start() {
-    dragSources = document.querySelectorAll(".drag");
-    dropTargets = document.querySelectorAll(".drop");
+    dragSources = [...document.querySelectorAll(".drag")];
+    dropTargets = [...document.querySelectorAll(".drop")];
     for (let dragSource of dragSources)
         dragSource.draggable = true;
     for (let dropTarget of dropTargets)
@@ -21,5 +22,24 @@ function dragOver(_event) {
 }
 function drop(_event) {
     let value = _event.dataTransfer.getData("value");
-    _event.target.value = value;
+    let target = _event.target;
+    target.value = value;
+}
+function change(_event) {
+    let variables = [...document.querySelectorAll("fieldset#variables div")];
+    for (let variable of variables) {
+        let name = variable.querySelector("input[name=name]");
+        let value = variable.querySelector("input[name=value]");
+        let type = variable.querySelector("select");
+        if (name.value && type.value) {
+            name.disabled = true;
+            type.disabled = true;
+            value.disabled = false;
+            value.classList.add("drop");
+            value.classList.add("drag");
+            value.addEventListener("dragover", dragOver);
+            dragSources.push(value);
+            dropTargets.push(value);
+        }
+    }
 }
--- Variables/Variables.ts
+++ Variables/Variables.ts
@@ -1,15 +1,16 @@
 window.addEventListener("load", start)
 window.addEventListener("dragstart", dragStart)
 window.addEventListener("drop", drop)
+window.addEventListener("change", change)
 
 type Input = HTMLInputElement
-type List = NodeListOf<Input>
+type List = Input[]
 let dropTargets: List;
 let dragSources: List;
 
 function start(): void {
-  dragSources = <List>document.querySelectorAll(".drag")
-  dropTargets = <List>document.querySelectorAll(".drop")
+  dragSources = <List>[...document.querySelectorAll(".drag")]
+  dropTargets = <List>[...document.querySelectorAll(".drop")]
 
   for (let dragSource of dragSources)
     dragSource.draggable = true
@@ -26,5 +27,24 @@ function dragOver(_event: DragEvent): void {
 }
 function drop(_event: DragEvent): void {
   let value: string = _event.dataTransfer!.getData("value");
-  (<Input>_event.target).value = value
+  let target: Input = <Input>_event.target
+  target.value = value
+}
+function change(_event: Event): void {
+  let variables: List = <List>[...document.querySelectorAll("fieldset#variables div")!]
+  for (let variable of variables) {
+    let name: HTMLInputElement = <HTMLInputElement>variable.querySelector("input[name=name]")!
+    let value: HTMLInputElement = <HTMLInputElement>variable.querySelector("input[name=value]")!
+    let type: HTMLSelectElement = <HTMLSelectElement>variable.querySelector("select")!
+    if (name.value && type.value) {
+      name.disabled = true
+      type.disabled = true
+      value.disabled = false
+      value.classList.add("drop")
+      value.classList.add("drag")
+      value.addEventListener("dragover", dragOver)
+      dragSources.push(value)
+      dropTargets.push(value)
+    }
+  }
 }
