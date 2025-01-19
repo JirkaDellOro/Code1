fd7adba37302829e2fbbf58e45b777481e897609
JirkaDellOro <del@hs-furtwangen.de>
2025-01-19 18:19:23 +0100

    infer simplified, operation included, queries centralized

--- Variables/Variables.js
+++ Variables/Variables.js
@@ -13,7 +13,7 @@ function start() {
         dragSource.draggable = true;
     for (let dropTarget of dropTargets)
         dropTarget.addEventListener("dragover", dragOver);
-    literal = document.querySelector("input[name=literal]");
+    literal = getInputByName("literal");
     literal.addEventListener("input", input);
 }
 function dragStart(_event) {
@@ -27,21 +27,42 @@ function drop(_event) {
     let value = _event.dataTransfer.getData("value");
     let target = _event.target;
     target.value = value;
+    change();
 }
 function input(_event) {
-    var _a;
-    let type = infer(literal.value);
-    ((_a = literal.parentElement) === null || _a === void 0 ? void 0 : _a.querySelector("input[name=type]")).value = type;
+    let type = typeof (convert(literal.value));
+    getInputByName("type", literal.parentElement).value = type;
     if (literal.value != "" && type == "")
         literal.setCustomValidity("unkown type");
     else
         literal.setCustomValidity("");
 }
 function change(_event) {
+    validateVariables();
+    operate();
+}
+function operate() {
+    let left = convert(getInputByName("left").value);
+    let right = convert(getInputByName("right").value);
+    let operator = document.querySelector("select[name=operator]").value;
+    let output = getInputByName("result");
+    let results = {
+        //@ts-ignore
+        "+": left + right, "-": left - right, "*": left * right, "/": left / right, "%": left % right
+    };
+    let result = results[operator];
+    if (typeof (result) == "string")
+        result = '"' + result + '"';
+    output.value = String(result);
+}
+function getInputByName(_name, _from = null) {
+    return (_from ? _from : document).querySelector(`input[name=${_name}]`);
+}
+function validateVariables() {
     let variables = [...document.querySelectorAll("fieldset#variables div")];
     for (let variable of variables) {
-        let name = variable.querySelector("input[name=name]");
-        let value = variable.querySelector("input[name=value]");
+        let name = getInputByName("name", variable);
+        let value = getInputByName("value", variable);
         let type = variable.querySelector("select");
         if (name.value && type.value) {
             name.disabled = true;
@@ -55,18 +76,25 @@ function change(_event) {
         }
     }
 }
-function infer(_value) {
-    if (_value == "true" || _value == "false")
-        return "boolean";
-    if (Number(_value).toString() === _value)
-        return "number";
-    let first = _value[0];
-    let last = _value[_value.length - 1];
-    let content = _value.slice(1, _value.length - 2);
-    if (first == last)
-        if (first == "'" || first == '"')
-            if (content.indexOf(first) == -1)
-                if (String(content).toString() === content)
-                    return "string";
-    return "";
+function convert(_value) {
+    try {
+        let parsed = JSON.parse(_value);
+        return parsed;
+    }
+    catch (_e) {
+        return undefined;
+    }
+    // if (_value == "true" || _value == "false")
+    //   return "boolean"
+    // if (Number(_value).toString() === _value)
+    //   return "number"
+    // let first: string = _value[0]
+    // let last: string = _value[_value.length - 1]
+    // let content: string = _value.slice(1, _value.length - 2)
+    // if (first == last)
+    //   if (first == "'" || first == '"')
+    //     if (content.indexOf(first) == -1)
+    //       if (String(content).toString() === content)
+    //         return "string"
+    // return ""
 }
--- Variables/Variables.ts
+++ Variables/Variables.ts
@@ -5,6 +5,7 @@ window.addEventListener("change", change)
 
 type Input = HTMLInputElement
 type List = Input[]
+type Types = string | number | boolean | undefined
 let dropTargets: List
 let dragSources: List
 let literal: Input
@@ -18,7 +19,7 @@ function start(): void {
   for (let dropTarget of dropTargets)
     dropTarget.addEventListener("dragover", dragOver)
 
-  literal = (<Input>document.querySelector("input[name=literal]"))
+  literal = getInputByName("literal");
   literal.addEventListener("input", input)
 }
 
@@ -35,11 +36,12 @@ function drop(_event: DragEvent): void {
   let value: string = _event.dataTransfer!.getData("value");
   let target: Input = <Input>_event.target
   target.value = value
+  change()
 }
 
 function input(_event: Event): void {
-  let type: string = infer(literal.value);
-  (<Input>literal.parentElement?.querySelector("input[name=type]")).value = type;
+  let type: string = typeof (convert(literal.value));
+  getInputByName("type", literal.parentElement).value = type;
   if (literal.value != "" && type == "")
     literal.setCustomValidity("unkown type");
   else
@@ -47,11 +49,36 @@ function input(_event: Event): void {
 
 }
 
-function change(_event: Event): void {
+function change(_event?: Event): void {
+  validateVariables();
+  operate();
+}
+
+function operate(): void {
+  let left: Types = convert(getInputByName("left").value);
+  let right: Types = convert(getInputByName("right").value);
+  let operator: string = (<Input>document.querySelector("select[name=operator]")!).value;
+  let output: Input = getInputByName("result");
+
+  let results: { [operator: string]: Types } = {
+    //@ts-ignore
+    "+": left + right, "-": left - right, "*": left * right, "/": left / right, "%": left % right
+  }
+  let result: Types = results[operator];
+  if (typeof(result) == "string")
+    result = '"' + result + '"';
+  output.value = String(result);
+}
+
+function getInputByName(_name: string, _from: HTMLElement | null = null): Input {
+  return <Input>(_from ? _from : document).querySelector(`input[name=${_name}]`);
+}
+
+function validateVariables(): void {
   let variables: List = <List>[...document.querySelectorAll("fieldset#variables div")!]
   for (let variable of variables) {
-    let name: Input = <Input>variable.querySelector("input[name=name]")!
-    let value: Input = <Input>variable.querySelector("input[name=value]")!
+    let name: Input = getInputByName("name", variable);
+    let value: Input = getInputByName("value", variable);
     let type: HTMLSelectElement = <HTMLSelectElement>variable.querySelector("select")!
     if (name.value && type.value) {
       name.disabled = true
@@ -66,20 +93,26 @@ function change(_event: Event): void {
   }
 }
 
-function infer(_value: string): string {
-  if (_value == "true" || _value == "false")
-    return "boolean"
-  if (Number(_value).toString() === _value)
-    return "number"
+function convert(_value: string): Types {
+  try {
+    let parsed: any = JSON.parse(_value)
+    return parsed;
+  } catch (_e) {
+    return undefined
+  }
+  // if (_value == "true" || _value == "false")
+  //   return "boolean"
+  // if (Number(_value).toString() === _value)
+  //   return "number"
 
-  let first: string = _value[0]
-  let last: string = _value[_value.length - 1]
-  let content: string = _value.slice(1, _value.length - 2)
-  if (first == last)
-    if (first == "'" || first == '"')
-      if (content.indexOf(first) == -1)
-        if (String(content).toString() === content)
-          return "string"
+  // let first: string = _value[0]
+  // let last: string = _value[_value.length - 1]
+  // let content: string = _value.slice(1, _value.length - 2)
+  // if (first == last)
+  //   if (first == "'" || first == '"')
+  //     if (content.indexOf(first) == -1)
+  //       if (String(content).toString() === content)
+  //         return "string"
 
-  return ""
+  // return ""
 }
