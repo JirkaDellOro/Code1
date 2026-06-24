ffc48c3430b9d6aaed7cd560e873d684e0702757
JirkaDellOro <del@hs-furtwangen.de>
2026-06-24 14:59:37 +0200

    interactively calculate NIM win

--- FirstCodeS26/FirstCode26.js
+++ FirstCodeS26/FirstCode26.js
@@ -1,5 +1,13 @@
 "use strict";
-let v = 1;
-v = v + "1";
-console.log(v);
+let z0 = parseInt(prompt("Input the number of lit lamps in row 0", "4"));
+let z1 = parseInt(prompt("Input the number of lit lamps in row 1", "7"));
+let z2 = parseInt(prompt("Input the number of lit lamps in row 2", "5"));
+let z3 = parseInt(prompt("Input the number of lit lamps in row 3", "3"));
+let win = calculateWin(z0, z1, z2, z3);
+console.log(win);
+alert("The indicator for a winning condition is: " + win);
+function calculateWin(_z0, _z1, _z2, _z3) {
+    let result = _z0 ^ _z1 ^ _z2 ^ _z3;
+    return result;
+}
 //# sourceMappingURL=FirstCode26.js.map
--- FirstCodeS26/FirstCode26.ts
+++ FirstCodeS26/FirstCode26.ts
@@ -1,3 +1,14 @@
-let v: number = 1;
-v = v + "1";
-console.log(v);
+let z0: number = parseInt(prompt("Input the number of lit lamps in row 0", "4")!);
+let z1: number = parseInt(prompt("Input the number of lit lamps in row 1", "7")!);
+let z2: number = parseInt(prompt("Input the number of lit lamps in row 2", "5")!);
+let z3: number = parseInt(prompt("Input the number of lit lamps in row 3", "3")!);
+
+let win: number = calculateWin(z0, z1, z2, z3);
+console.log(win);
+alert("The indicator for a winning condition is: " + win);
+
+function calculateWin(_z0: number, _z1: number, _z2: number, _z3: number): number {
+  let result: number = _z0 ^ _z1 ^ _z2 ^ _z3;
+  return result;
+}
+
